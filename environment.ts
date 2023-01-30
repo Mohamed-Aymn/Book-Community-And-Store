151733cbeda629642b544b4f2bcdcc71c16a64a3
Mohamed Aymn <mohamedaymn218@gmail.com>
2023-01-30 18:52:00 +0200

    fis: env issue.

--- environment.ts
+++ environment.ts
@@ -1,5 +1,16 @@
 import packageInfo from "./package.json";
 
+const vercel_public_domain = process.env.NEXT_PUBLIC_DOMAIN;
+
+const node_prod_env = process.env.NODE_ENV === "production";
+
+// if Vercel prod deployment, then use main domain; else use temporary domain unless you're on dev
+export const DOMAIN = vercel_public_domain
+    ? vercel_public_domain
+    : node_prod_env
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : "http://127.0.0.1:3000";
+
 let baseUrl;
 process.env.NODE_ENV === "development"
     ? (baseUrl = "http://localhost:3000")
