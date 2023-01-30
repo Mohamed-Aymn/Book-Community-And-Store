5cd539b84f3626185220dd3210a261244f6208f9
Mohamed Aymn <mohamedaymn218@gmail.com>
2023-01-30 16:41:40 +0200

    fix: base url env object

--- environment.ts
+++ environment.ts
@@ -1,8 +1,8 @@
 import packageInfo from "./package.json";
 
 let baseUrl;
-process.env.NODE_ENV === "production"
-    ? (baseUrl = process.env.NEXT_PUBLIC_BASE_URL)
+process.env.NODE_ENV == "production"
+    ? (baseUrl = process.env.VERCEL_URL)
     : (baseUrl = "http://localhost:3000");
 
 export const env = {
