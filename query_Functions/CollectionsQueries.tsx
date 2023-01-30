d92e2d4952f16ec3a2e5d855d19a1ba3f228eef8
Mohamed Aymn <mohamedaymn218@gmail.com>
2023-01-30 18:29:57 +0200

    fix: env file issue

--- query_Functions/CollectionsQueries.tsx
+++ query_Functions/CollectionsQueries.tsx
@@ -1,16 +1,16 @@
 import { env } from "../environment";
 
 export const getFreeBooks = async () => {
-    return fetch(`${env.BASE_URL}/api/books?collection=free-ebooks`).then(
-        async (res) => {
-            let data = await res.json();
-            return data.data.items;
-        }
-    );
+    return fetch(
+        `https://${env.BASE_URL}/api/books?collection=free-ebooks`
+    ).then(async (res) => {
+        let data = await res.json();
+        return data.data.items;
+    });
 };
 
 export const getEbooks = async () => {
-    return fetch(`${env.BASE_URL}/api/books?collection=ebooks`).then(
+    return fetch(`https://${env.BASE_URL}/api/books?collection=ebooks`).then(
         async (res) => {
             let data = await res.json();
             return data.data.items;
