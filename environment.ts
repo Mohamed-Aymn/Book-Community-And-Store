546d63e7ba89a35978d53081eb02f7fb3fa00086
Mohamed Aymn <mohamedaymn218@gmail.com>
2023-01-30 18:06:35 +0200

    fix: env file third trial

--- environment.ts
+++ environment.ts
@@ -1,12 +1,12 @@
 import packageInfo from "./package.json";
 
-let baseUrl;
-process.env.NODE_ENV === "production"
-    ? (baseUrl = process.env.BASE_URL)
-    : (baseUrl = "http://localhost:3000");
+// let baseUrl;
+// process.env.NODE_ENV === "production"
+//     ? (baseUrl = process.env.VERCEL_URL)
+//     : (baseUrl = "http://localhost:3000");
 
 export const env = {
     production: process.env.NODE_ENV === "production",
-    BASE_URL: baseUrl,
+    BASE_URL: process.env.VERCEL_URL,
     VERSION: packageInfo.version,
 };
