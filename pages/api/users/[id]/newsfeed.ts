ac8150f6269107b1b7cfadbc73473b6fd4ddfc02
Mohamed Aymn <mohamedaymn218@gmail.com>
2023-02-19 02:33:56 +0200

    feat: manipulate lack of data in newsfeed

--- pages/api/users/[id]/newsfeed.ts
+++ pages/api/users/[id]/newsfeed.ts
@@ -1,7 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import dbConnect from "../../../../lib/dbConnect";
-import Book from "../../../../models/Book";
-import User from "../../../../models/User";
 import Review from "../../../../models/Review";
 
 interface Data {
@@ -15,47 +13,15 @@ export default async function handler(
     res: NextApiResponse<Data>
 ) {
     const { query, method } = req;
-    let id = query.id;
     await dbConnect();
 
     switch (method) {
         case "GET":
             try {
-                const user = await User.findById(id);
-
-                if (user.following.length === 0) {
-                    res.status(200).json({
-                        message:
-                            "there is nothing to show, please follow some people",
-                    });
-                    return;
-                }
-
-                let newsfeed: any[] = [];
-
                 const reviews = await Review.find({});
-                reviews.map((review) => {
-                    // search for reviews about the same book
-                    user.readBooks.map((book: string) => {
-                        if (book === review.book) newsfeed.push(review);
-                    });
-
-                    // search for reviews created be followees
-                    user.following.map((followee: string) => {
-                        if (followee === review.reviwer) newsfeed.push(review);
-                    });
-                });
-
-                if (newsfeed.length === 0) {
-                    res.status(200).json({
-                        message:
-                            "unfortunately there is no relevant news feed for you, try to show your intresets in your fields.",
-                    });
-                    return;
-                }
 
                 res.status(200).json({
-                    data: newsfeed,
+                    data: reviews,
                 });
             } catch (error) {
                 let message = (error as Error).message;
